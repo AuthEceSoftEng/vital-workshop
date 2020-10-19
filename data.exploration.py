794443841e7d6bbd20ab9fa704cc2b20f4a0c7a8
thdiaman <themisdiamantopoulos@hotmail.com>
2020-10-19 15:55:13 +0300

    Add also py files based on notebooks

---
+++ data.exploration.py
@@ -0,0 +1,276 @@
+# %%
+"""
+# An introduction in processing and visualization of IoT data
+"""
+
+# %%
+"""
+## 1. Getting the data via HTTP GET request
+"""
+
+# %%
+"""
+### 1.1 Definition of keys and API access point
+
+cenote Documentation:
+
+http://assist.ee.auth.gr/docs/
+"""
+
+# %%
+PROJECT   = 'pide23f7ea9d71'
+READ_KEY  = 'c55787bd-f262-413a-abb5-dfd07896ef75'
+WRITE_KEY = 'bdaf39d4-6f58-4f40-a953-5c285c26ea94'
+
+PROJECT   = 'pidc06ebaeb8e6'
+READ_KEY  = 'd5c81802-a1f6-46a2-ad31-8be10d7172f4'
+WRITE_KEY = 'cc5d8392-1226-42c6-a1e2-2d4555a67682'
+
+URL = "https://cenote.sidero.services/api/projects/" + PROJECT
+
+# %%
+"""
+### 1.2 Define and test the functions
+"""
+
+# %%
+"""
+We begin by defining an abstract function that performs a GET request.
+
+To account for the different type of queries, this function is query type agnostic.
+It thus passes the type of query and the query arguments to the GET request.
+Should the query by unsuccessful, a RuntimeError is raised.
+"""
+
+# %%
+import requests
+
+def cenote_get(event_name, query_type, query_dict={}):
+    get_url = "{0}/queries/{1}".format(URL, query_type)
+    query_dict["readKey"] = READ_KEY
+    query_dict["event_collection"] = event_name
+    r = requests.get(get_url, query_dict)
+    r = dict(r.json())
+    if r["ok"]:
+        return r["results"]
+    else:
+        raise RuntimeError("Query unsuccessful due to {0}. Message is: {1}".format(
+            r["results"], r["message"]))
+
+# %%
+"""
+We test the efficacy of the function on known data and known unknowns (:
+"""
+
+# %%
+cenote_get("measurements", "extraction", {"latest": 1})
+
+# %%
+cenote_get("NOT_EXISTING", "extraction", {"latest": 2})
+
+# %%
+"""
+**Cenote also supports some higher level quering ...**
+"""
+
+# %%
+cenote_get("measurements", "minimum", {"target_property": "temperature"})
+
+# %%
+"""
+### 1.3 Get and store the data in a dataframe
+"""
+
+# %%
+"""
+pandas is a fast, powerful, flexible and easy to use open source data analysis and manipulation tool.
+
+It provides:
+
+* a fast and efficient **DataFrame** object for data manipulation with integrated indexing;
+* tools for reading and writing data between in-memory data structures and different formats: CSV and text files, Microsoft Excel, SQL databases, and the fast HDF5 format;
+* flexible reshaping and pivoting of data sets;
+* intelligent label-based slicing, fancy indexing, and subsetting of large data sets
+
+
+A quick [guide may be found here](https://pandas.pydata.org/docs/user_guide/index.html#user-guide), with the complete [API reference being provided here](https://pandas.pydata.org/docs/reference/index.html)
+"""
+
+# %%
+import pandas
+
+# Custom filter on extraction to get data from a given sensor only
+cus_filter = '[{"property_name":"un","operator":"eq","property_value":"ADU-700HP_V2"}]'
+res = cenote_get("measurements", "extraction", {"filters": cus_filter, "latest": 50000})
+
+# Transform query result to a pandas DataFrame
+df = pandas.DataFrame(res)
+df = df.sort_values(by="epoch").drop_duplicates(subset="epoch").reset_index(drop=True)
+df
+
+# %%
+"""
+## 2. Initial data exploration
+"""
+
+# %%
+"""
+Now is a good idea to gauge what kind of data are stored in the DataFrame.
+
+To this end, we explore the columns and the types of data stored.
+"""
+
+# %%
+df.columns
+
+# %%
+df.dtypes
+
+# %%
+"""
+**Some of these data types may be auto-magically corrected ...**
+"""
+
+# %%
+df = df.convert_dtypes()
+df.dtypes
+
+# %%
+"""
+**While for others, we need to manually fix them ...**
+"""
+
+# %%
+keys = ['solar', 'precipitation', 'windspeed', 'winddirection', 'temperature', 'humidity', 
+        'cntv2', 'vwc1', 'temp1', 'ec1', 'vwc2', 'temp2', 'ec2', 'analogv1', 'cntv1',
+        'analogv3', 'cntv3', 'cntvm']
+
+for key in keys:
+    df[key] = pandas.to_numeric(df[key], errors="coerce")
+
+df.epoch = pandas.to_datetime(df.epoch, unit='ms')
+
+df.dtypes
+
+# %%
+"""
+**We can also very easily see some statistical moments that describe our data**
+"""
+
+# %%
+df.describe()
+
+# %%
+"""
+Some outliers may then be easily corrected.
+
+Here, we use the .loc function which provides quick indexing of the data, in order to select the subsets fulfilling a given condition.
+"""
+
+# %%
+import numpy as np
+
+df.loc[df.temperature < -100, ('temperature')] = np.nan
+df.loc[df.humidity < 0, ('humidity')] = np.nan
+
+# %%
+"""
+## 3. Visualization of data
+"""
+
+# %%
+"""
+An important aspect of data processing is visualization.
+
+Most of the work here utilizes the [matplotlib](https://matplotlib.org/) package beneath the hoods. We will use it on a higher level through pandas and through another library called [seaborn](https://seaborn.pydata.org/).
+"""
+
+# %%
+"""
+### 3.1 Line plot and some initializations
+"""
+
+# %%
+df.plot(x="epoch", y="temperature") # kind="line"
+
+# %%
+df.plot(x="epoch", y="temperature", figsize=(11.69, 8.27))
+
+# %%
+import matplotlib.pyplot as plt
+
+plt.style.use("ggplot")
+
+# %%
+df.plot(x="epoch", y="temperature", figsize=(11.69, 8.27))
+
+# %%
+"""
+### 3.2 Boxplots
+"""
+
+# %%
+df['per'] = df.set_index("epoch").index.to_period('W')
+
+df.boxplot(column="temperature", by="per", figsize=(11.69, 8.27), rot=90)
+
+# %%
+df['per'] = df.set_index("epoch").index.to_period('M')
+
+df.boxplot(column="temperature", by="per", figsize=(11.69, 8.27))
+
+# %%
+"""
+### 3.3 Barplots
+"""
+
+# %%
+"""
+It is quite easy to group data per week ...
+"""
+
+# %%
+df.set_index("epoch").precipitation.resample("W").sum().plot(kind="bar", figsize=(11.69, 8.27))
+
+# %%
+"""
+### 3.3 Using seaborn
+"""
+
+# %%
+import seaborn
+
+seaborn.set_palette("colorblind")
+
+# %%
+"""
+The DataFrame object is based on the tools used in the R programming language.
+
+There, lots of functions expect the data to be in the "long format". The seaborn library follows this approach, too. To this end we are going to manipulate our data through pandas before feeding it into seaborn. 
+"""
+
+# %%
+sdf = pandas.wide_to_long(
+    df=df.drop("cntv3", axis=1),
+    stubnames=["vwc", "temp", "ec", "cntv"],
+    i="epoch",
+    j="sensor"
+)
+sdf
+
+# %%
+sdf = sdf.reset_index().melt(
+    id_vars=["epoch", "sensor"],
+    value_vars=["vwc", "temp", "ec", "cntv"]
+)
+sdf
+
+# %%
+sdf['per'] = sdf.set_index("epoch").index.to_period('M')
+
+# %%
+g = seaborn.catplot(
+    x="per", y="value",
+    hue="sensor", col="variable",
+    data=sdf, kind="box",
+    height=7, aspect=1, col_wrap=2, sharey=False);
---
+++ intro-to-machine-learning.py
@@ -0,0 +1,203 @@
+# %%
+PROJECT   = 'pide23f7ea9d71'
+READ_KEY  = 'd5c81802-a1f6-46a2-ad31-8be10d7172f4'
+
+URL = "https://cenote.sidero.services/api/projects/" + PROJECT
+
+import requests
+
+def cenote_get(event_name, query_type, query_dict={}):
+    get_url = "{0}/queries/{1}".format(URL, query_type)
+    query_dict["readKey"] = READ_KEY
+    query_dict["event_collection"] = event_name
+    r = requests.get(get_url, query_dict)
+    r = dict(r.json())
+    if r["ok"]:
+        return r["results"]
+    else:
+        raise RuntimeError("Query unsuccessful due to {0}. Message is: {1}".format(
+            r["results"], r["message"]))
+        
+import pandas
+
+# Custom filter on extraction to get data from a given sensor only
+cus_filter = '[{"property_name":"un","operator":"eq","property_value":"ADU-700HP_V2"}]'
+res = cenote_get("measurements", "extraction", {"filters": cus_filter, "latest": 50000})
+
+# Transform query result to a pandas DataFrame
+df = pandas.DataFrame(res)
+df = df.sort_values(by="epoch").drop_duplicates(subset="epoch").reset_index(drop=True)
+
+# Convert keys
+df = df.convert_dtypes()
+keys = ['solar', 'precipitation', 'windspeed', 'winddirection', 'temperature', 'humidity', 
+        'cntv2', 'vwc1', 'temp1', 'ec1', 'vwc2', 'temp2', 'ec2', 'analogv1', 'cntv1',
+        'analogv3', 'cntv3', 'cntvm']
+
+for key in keys:
+    df[key] = pandas.to_numeric(df[key], errors="coerce")
+
+df.epoch = pandas.to_datetime(df.epoch, unit='ms')
+
+import numpy as np
+
+df.loc[df.temperature < -100, ('temperature')] = np.nan
+for key in ["windspeed", "winddirection", "temperature", "humidity"]:
+    df.loc[df[key] < 0, (key)] = np.nan
+
+# %%
+df
+
+# %%
+df.columns
+
+# %%
+"""
+# Weather forecast
+"""
+
+# %%
+"""
+## Build a meteo DataFrame
+"""
+
+# %%
+"""
+### Select only the relevant features (columns)
+"""
+
+# %%
+meteo_df = df[['epoch', 'solar', 'precipitation', 'windspeed', 'winddirection', 'humidity', 'temperature']]
+meteo_df
+
+# %%
+"""
+### Resample to 3 hours
+"""
+
+# %%
+meteo_df = meteo_df.set_index("epoch").resample("3H").mean()
+meteo_df
+
+# %%
+"""
+## Build a dataset --- predict given the current settings the temperature in 3 hours!
+"""
+
+# %%
+dataset_df = meteo_df.copy(deep=True)
+
+# %%
+"""
+### Extra feature: hour in 24 hour format
+"""
+
+# %%
+dataset_df.loc[:, ("hour")] = [obj.hour for obj in meteo_df.index.time]
+dataset_df
+
+# %%
+"""
+### Add a column with the future temperature (the target variable)
+"""
+
+# %%
+future_temp = dataset_df.temperature.tolist()
+# Delete the first element to offset all by one ...
+future_temp = future_temp[1:]
+# Delete the last row of dataset_df as we have no target for it ...
+dataset_df.drop(dataset_df.tail(1).index,inplace=True)
+# And now add the target!
+dataset_df.loc[:, ('future_temperature')] = future_temp
+dataset_df
+
+# %%
+dataset_df = dataset_df.dropna()
+dataset_df
+
+# %%
+import matplotlib.pyplot as plt
+
+f = plt.figure(figsize=(11.69, 8.27))
+plt.matshow(dataset_df.corr(), fignum=f.number)
+plt.xticks(range(dataset_df.shape[1]), dataset_df.columns, fontsize=14, rotation=45)
+plt.yticks(range(dataset_df.shape[1]), dataset_df.columns, fontsize=14)
+cb = plt.colorbar()
+
+# %%
+"""
+## Use scikit-learn to build a model
+
+
+[sklearn](https://scikit-learn.org/stable/supervised_learning.html#supervised-learning) is an open source machine learning library that supports supervised and unsupervised learning. It also provides various tools for model fitting, data preprocessing, model selection and evaluation, and many other utilities.
+"""
+
+# %%
+"""
+### Create numpy arrays 
+"""
+
+# %%
+X = dataset_df.loc[:, dataset_df.columns != 'future_temperature'].to_numpy()
+y = dataset_df.loc[:, dataset_df.columns == 'future_temperature'].to_numpy().flatten()
+y
+
+# %%
+"""
+### Split the data to training and testing
+"""
+
+# %%
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=2020)
+
+# %%
+print("Training size: {0}\n Testing size: {1}".format(
+    len(y_train), len(y_test)))
+
+# %%
+"""
+### Build a model
+"""
+
+# %%
+from sklearn.linear_model import LinearRegression
+
+reg = LinearRegression().fit(X_train, y_train)
+
+y_hat = reg.predict(X_test)
+
+# %%
+"""
+### Validate --- Model assessment
+"""
+
+# %%
+from sklearn.metrics import r2_score, mean_squared_error
+from math import sqrt
+
+print("RMSE: {0:.2f}, R2: {1:.2f}".format( 
+      sqrt(mean_squared_error(y_true=y_test, y_pred=y_hat)),
+      r2_score(y_true=y_test, y_pred=y_hat)
+     ))
+
+# %%
+import matplotlib.pyplot as plt
+
+plt.plot(y_test, y_hat, 'o')
+
+axis_min = min(min(y_test), min(y_hat))
+axis_max = max(max(y_test), max(y_hat))
+
+plt.scatter(y_test, y_hat, linestyle="None", marker="o")
+plt.xlim(axis_min, axis_max)
+plt.ylim(axis_min, axis_max)
+plt.xlabel("Observed")
+plt.ylabel("Predicted")
+plt.plot([axis_min, axis_max], [axis_min, axis_max], ls="--", c=".3")
+m, b = np.polyfit(y_test, y_hat, 1)
+step = (axis_max - axis_min ) / 100
+x_lsq = np.arange(axis_min, axis_max+step, step)
+plt.plot(x_lsq, [i * m + b for i in x_lsq], '-')
